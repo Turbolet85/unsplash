a138e80c7a6d547dcb54bf8805fcb29eba08f4d5
Anton Soldatov <turbolet85@gmail.com>
2023-08-05 19:33:33 +0200

    entry

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import styles from './app.module.css';
+
+const App = () => {
+  return (
+    <main className={styles.container}>
+      <div>BOILERPLATE</div>
+    </main>
+  );
+};
+
+export default App;
